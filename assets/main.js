4de9f90102411f079b32ecd1d889e43c5d6ee37a
cf-umeda <cf.umeda.web@gmail.com>
2022-11-10 17:09:02 +0900

    Initial Commit

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+console.log("a");
+  $(".slider").slick({
+    autoplay: true, 
+    autoplaySpeed:5000,
+    speed:300,
+    dots: true,
+    arrows: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    cssEase:'linear',
+  });
